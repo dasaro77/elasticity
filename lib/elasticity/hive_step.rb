8b2ba4576adcbcb5f588c142b45883b1793855a0
David Rom <drom@mangohealth.com>
2015-02-20 11:17:07 -0800

    Allow requested hive version to be dynamic sine 'latest' == '0.11.0.2' in the current hive-script and 0.13.1 is the actual current latest for EMR AMI 3.3.2

--- lib/elasticity/hive_step.rb
+++ lib/elasticity/hive_step.rb
@@ -8,16 +8,18 @@ module Elasticity
     attr_accessor :script
     attr_accessor :variables
     attr_accessor :action_on_failure
+    attr_accessor :version
 
     def initialize(script)
       @name = "Elasticity Hive Step (#{script})"
       @script = script
       @variables = {}
       @action_on_failure = 'TERMINATE_JOB_FLOW'
+      @version = 'latest'
     end
 
     def to_aws_step(_)
-      args = %w(s3://elasticmapreduce/libs/hive/hive-script --base-path s3://elasticmapreduce/libs/hive/ --hive-versions latest --run-hive-script --args)
+      args = "s3://elasticmapreduce/libs/hive/hive-script --base-path s3://elasticmapreduce/libs/hive/ --hive-versions #{version} --run-hive-script --args"
       args.concat(['-f', @script])
       @variables.keys.sort.each do |name|
         args.concat(['-d', "#{name}=#{@variables[name]}"])
@@ -46,7 +48,7 @@ module Elasticity
           :action_on_failure => 'TERMINATE_JOB_FLOW',
           :hadoop_jar_step => {
             :jar => 's3://elasticmapreduce/libs/script-runner/script-runner.jar',
-            :args => %w(s3://elasticmapreduce/libs/hive/hive-script --base-path s3://elasticmapreduce/libs/hive/ --install-hive --hive-versions latest)
+            :args => "s3://elasticmapreduce/libs/hive/hive-script --base-path s3://elasticmapreduce/libs/hive/ --install-hive --hive-versions #{version}"
           },
           :name => aws_installation_step_name
         }
@@ -63,7 +65,7 @@ module Elasticity
               '--install-hive-site',
               "--hive-site=#{Elasticity.configuration.hive_site}",
               '--hive-versions',
-              'latest'
+              version
             ],
           },
           :name => 'Elasticity - Configure Hive via Hive Site'
